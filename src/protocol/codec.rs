3a93625f876b2e39a2c6dea43b7c7342609f248d
Alezito2008 <>
2026-03-29 17:00:40 -0300

    Agregadas mas conversiones y tests

--- src/protocol/codec.rs
+++ src/protocol/codec.rs
@@ -1,24 +1,28 @@
 use crate::protocol::{Direction, FilterResult, Packet, varint::read_varint};
 
 pub fn inspect_packet(buffer: &mut Vec<u8>, dir: &Direction) -> FilterResult {
-    let (size, len_size) = match read_varint(&buffer) {
+    // Leer largo total del paquete
+    let (total_length, len_size) = match read_varint(&buffer) {
         Some(v) => v,
         None => return FilterResult::Incomplete
     };
 
-    let total_size = len_size + size as usize;
-    if buffer.len() < total_size {
+    let total_packet_size = len_size + total_length as usize;
+    if buffer.len() < total_packet_size {
         return FilterResult::Incomplete
     }
 
-    let raw_packet = buffer.drain(..total_size).collect::<Vec<u8>>();
-    let (packet_id, id_size) = read_varint(&raw_packet).unwrap();
+    let raw_packet = buffer.drain(..total_packet_size).collect::<Vec<u8>>();
+    let (packet_id, id_size) = read_varint(&raw_packet[len_size..]).unwrap();
 
     let _packet = Packet {
         id: packet_id,
         data: raw_packet[len_size + id_size..].to_vec()
     };
 
-    println!("[{dir}] ID: 0x{:02X}", packet_id);
+    if packet_id < 0x10 && packet_id != 0x0 {
+        println!("[{dir}] ID: 0x{:02X}", packet_id);
+    }
+
     FilterResult::Send(raw_packet)
 }
--- src/protocol/varint.rs
+++ src/protocol/varint.rs
@@ -21,3 +21,83 @@ pub fn read_varint(buf: &[u8]) -> Option<(i32, usize)> {
 
     None
 }
+
+pub fn write_varint(mut value: i32, buf: &mut Vec<u8>) {
+    let mut u_value = value as u32;
+
+    loop {
+        let mut byte = (u_value & 0b01111111) as u8;
+        u_value >>= 7;
+        if u_value != 0 {
+            byte |= 0b10000000;
+        }
+        buf.push(byte);
+        if u_value == 0 {
+            break;
+        }
+    }
+}
+
+pub fn read_string(buf: &[u8]) -> Option<(String, usize)> {
+    let (length, len_size) = read_varint(buf)?;
+    let total_size = len_size + length as usize;
+
+    if buf.len() < total_size {
+        return None;
+    }
+
+    let string_bytes = &buf[len_size..total_size];
+    return Some((String::from_utf8_lossy(string_bytes).to_string(), total_size));
+}
+
+pub fn write_string(text: &str, buf: &mut Vec<u8>) {
+    let length = text.len() as i32;
+
+    write_varint(length, buf);
+    buf.extend_from_slice(text.as_bytes());
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::protocol::varint::{read_string, read_varint, write_string, write_varint};
+
+    #[test]
+    fn test_read_single_byte() {
+        let data = [0x05];
+        let (val, size) = read_varint(&data).unwrap();
+        assert_eq!(val, 5);
+        assert_eq!(size, 1)
+    }
+
+    #[test]
+    fn test_read_multi_byte() {
+        let data = [0xff, 0xff, 0x7f];
+        let (val, size) = read_varint(&data).unwrap();
+        assert_eq!(val, 2097151);
+        assert_eq!(size, 3);
+    }
+
+    #[test]
+    fn test_incomplete_varint() {
+        // Si el primer bit es 1 pero no hay más bytes, debe dar None
+        let data = [0x80]; 
+        assert!(read_varint(&data).is_none());
+    }
+
+    #[test]
+    fn test_write_varint() {
+        let mut buf: Vec<u8> = Vec::new();
+        write_varint(2097151, &mut buf);
+        assert_eq!(buf, vec![0xff, 0xff, 0x7f])
+    }
+
+    #[test]
+    fn test_string_codec() {
+        let mut buf = Vec::new();
+        write_string("Test", &mut buf);
+
+        let (result, size) = read_string(&buf).unwrap();
+        assert_eq!(result, "Test");
+        assert_eq!(size, 5); // 1 byte de varint + 4 de texto
+    }
+}
