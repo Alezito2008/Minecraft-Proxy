ef7ec1bb17eb82a8b9bc3607f51b1af637f019ac
Alezito2008 <>
2026-03-31 11:57:11 -0300

    Basic configuration

--- src/protocol/packets/configuration.rs
+++ src/protocol/packets/configuration.rs
@@ -6,17 +6,38 @@ use self::packets::*;
 pub struct ConfigurationHandler;
 
 impl PacketHandler for ConfigurationHandler {
-    fn handle_c2s(_reader: &mut PacketReader, _id: i32, _state: &mut ConnectionState) {
-        
+    fn handle_c2s(_reader: &mut PacketReader, id: i32, state: &mut ConnectionState) {
+        match id {
+            AcknowledgeFinishConfiguration::ID => {
+                println!("Acknowledge finish configuration");
+                *state = ConnectionState::Play;
+            }
+            _ => {}
+        }
     }
 
-    fn handle_s2c(_reader: &mut PacketReader, _id: i32, _state: &mut ConnectionState) {
-        
+    fn handle_s2c(_reader: &mut PacketReader, id: i32, _state: &mut ConnectionState) {
+        match id {
+            FinishConfiguration::ID => {
+                println!("Finish configuration");
+            }
+            _ => {}
+        }
     }
 }
 
 pub mod packets {
     use super::*;
-    use crate::protocol::varint::*;
     
+    pub struct FinishConfiguration;
+
+    impl MinecraftPacket for FinishConfiguration {
+        const ID: i32 = 0x03;
+    }
+
+    pub struct AcknowledgeFinishConfiguration;
+
+    impl MinecraftPacket for AcknowledgeFinishConfiguration {
+        const ID: i32 = 0x03;
+    }
 }
